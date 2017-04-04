fb3b34deabe296f87d1ce3c45fbf9591264b4a21
David Esposito <david@espo.coffee>
2017-04-04 01:13:36 -0400

    Styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Demo from './components/Demo'
 
 const App = () => {
   return (
-    <div className="App container">
+    <div className="App container" style={{ marginBottom: "3em" }}>
     	<Demo />
     </div>
   )
